caead08f324eaf8898741d7a5ac47968d7683a5f
Sancho Amaral Simões <ssimoes@ipn.pt>
2021-05-24 15:35:41 +0100

    MAIN: Add tcp and udp files.

--- MessageXChanger/server.c
+++ MessageXChanger/server.c
@@ -1,8 +1,11 @@
+#include "stdlib.h"
 #include "server.h"
 #include "helpers/client_reg_file_manager/client_reg_file_manager.h"
 
 int main(void) {
-    client_reg_reader_init(CLIENT_REG_FILE_PATH, CLIENT_REG_FILE_PATH_B);
+    node_t * users = NULL;
 
-    
+    client_reg_reader_init(CLIENT_REG_FILE_PATH, CLIENT_REG_FILE_PATH_B);
+    users = read_client_regs();
+
 }
---
+++ MessageXChanger/tcp_ip/tcp/tcp.c
@@ -0,0 +1,5 @@
+//
+// Created by sancho on 24/05/21.
+//
+
+#include "tcp.h"
